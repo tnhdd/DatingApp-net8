f59000d52a420b0ec196bd0d8ba0636372484b81
tnhdd <tuandod21@gmail.com>
2025-01-13 17:33:49 +0100

    Section 6: Routing in Angular

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,14 +14,14 @@ export class RegisterComponent {
   cancelRegister = output<boolean>();
   model: any = {};
   private accountServices = inject(AccountService);
-
+  private toastr = inject(ToastrService)
   register() {
     this.accountServices.register(this.model).subscribe({
       next: (response) => {
         console.log(response);
         this.cancel();
       },
-      error: (error) => console.log(error),
+      error: (error) => this.toastr.error(error.error),
     });
   }
 
